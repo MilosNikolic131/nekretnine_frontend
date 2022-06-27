237d629fc847b4d104a926d0887e576b20e47f1a
MilosNikolic131 <milosnikolic99x@gmail.com>
2022-06-27 22:08:09 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
 
+import Navbar from './components/navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Pocetna from './components/pocetna';
+import Login from './components/login';
+import { useState } from "react";
+import Agenti from './components/agenti';
+import ZakaziObilazak from './components/zakazivanje';
+import Nekretnine from './components/nekretnine';
+import DodavanjeNekretnine from './components/dodavanjeNekretnine';
+import DodavanjeAgenta from './components/dodavanjeAgenta';
+import LoginAgent from './components/loginAgent';
+import DodavanjeKorisnika from './components/registerKorisnik';
 function App() {
+  const [token, setToken] = useState();
+  function addToken(auth_token) {
+    setToken(auth_token);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar token = {token}></Navbar>
+      <div className="content">
+        <Routes>
+          <Route exact path="/" element={<Pocetna />} />
+          <Route exact path="/agenti" element={<Agenti />} />
+          <Route exact path="/login" element={<Login addToken={addToken} />} />
+          <Route exact path="/dodavanjeagenta" element={<DodavanjeAgenta/>} />
+          <Route exact path="/registerkorisnik" element={<DodavanjeKorisnika/>} />
+          <Route exact path="/zakazivanje" element={<ZakaziObilazak />} />
+          <Route exact path="/nekretnine" element={<Nekretnine />} />
+          <Route exact path="/dodavanjenekretnina" element={<DodavanjeNekretnine />} />
+          <Route exact path="/loginAgent" element={<LoginAgent addToken={addToken}/>} />
+        </Routes>
+
+      </div>
+    </Router>
+
   );
 }
 
